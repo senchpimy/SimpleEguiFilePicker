0a6487ea4577e208c95ccb754536ff95a4f47127
senchi <senchpimy@gmail.com>
2022-12-03 10:49:36 -0600

    finally a working version

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,29 @@ use eframe::{egui::{CentralPanel,ScrollArea,TopBottomPanel,Ui,Context}, run_nati
 use std::fs;
 use std::fs::metadata;
 use egui::widgets::Link;
-//use std::sync::mpsc;
-//use std::thread;
+use directories::BaseDirs;
 
 const APP_NAME: &str = "Files";
 
 struct MyFiles {
     path:String,
     prev_path:String,
-    search:String
+    search:String,
 }
 
 impl MyFiles {
     fn new()-> MyFiles {
-        if cfg!(windows) {
             Self {
-            path:String::from("/"),
-            prev_path:String::from("/"),
-            search:"".to_owned(),
-            }
-        }else if cfg!(unix){
-            Self {
-            path:String::from("/"),
-            prev_path:String::from("/"),
-            search:"".to_owned(),
-            }
-        }else if cfg!(target_os = "macos"){
-            Self {
-            path:String::from("/"),
-            prev_path:String::from("/"),
-            search:"".to_owned(),
-            }
-        }else{
-            Self {
-            path:String::from("/"),
+            path:BaseDirs::new().unwrap().home_dir().to_str().unwrap().to_string(),
             prev_path:String::from("/"),
             search:"".to_owned(),
             }
 
-        }
     }
 }
 
 impl eframe::App for MyFiles{
-    fn update(&mut self, ctx:  &Context, _frame: &mut eframe::Frame){
+    fn update(&mut self, ctx:  &Context, frame: &mut eframe::Frame){
         CentralPanel::default().show(ctx, |ui| {
             if ui.button("return").clicked(){
                 self.path=self.prev_path.to_owned();
@@ -56,10 +35,10 @@ impl eframe::App for MyFiles{
                     let linku = Link::new(file_path.to_owned());
 
                     if self.search.len()==0{
-                        render_files(file_path,linku,ui,&mut self.prev_path,&mut self.path);
+                        render_files(file_path,linku,ui,&mut self.prev_path,&mut self.path,frame);
                     }else{
                         if file_path.contains(&self.search){
-                        render_files(file_path,linku,ui,&mut self.prev_path, &mut self.path);
+                        render_files(file_path,linku,ui,&mut self.prev_path, &mut self.path, frame);
                         }
                     }}
             });
@@ -71,17 +50,16 @@ impl eframe::App for MyFiles{
 }
 
 pub fn run_window(){
-
+    
     let options = NativeOptions::default();
     let app = MyFiles::new();
-    run_native(
-        APP_NAME,
-        options,
-        Box::new(|_cc| Box::new(app)),
-    );
+        run_native(
+         APP_NAME,
+         options,
+         Box::new(|_cc| Box::new(app)),
+      );
 
-
-}
+    }
 
 fn is_dir(value:String)->bool{
     let md = metadata(value).unwrap();
@@ -92,7 +70,7 @@ fn is_dir(value:String)->bool{
     }
 }
 
-fn render_files(file_path:String, linku:Link, ui:&mut Ui, prev_path:&mut String, path:&mut String){
+fn render_files(file_path:String, linku:Link, ui:&mut Ui, prev_path:&mut String, path:&mut String,frame: &mut eframe::Frame){
     if is_dir(file_path.to_owned()){
         if ui.add(linku).clicked(){
             *prev_path = path.to_owned();
@@ -101,7 +79,8 @@ fn render_files(file_path:String, linku:Link, ui:&mut Ui, prev_path:&mut String,
         ui.separator();
     }else{
         if ui.add(linku).clicked(){
-            println!("{}",file_path);
+            print!("{}",file_path);
+            frame.close();
         }
         ui.separator();
     }
