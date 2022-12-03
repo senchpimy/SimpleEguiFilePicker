548d98babde800b29187decbb3d2e73c26a4b419
senchi <senchpimy@gmail.com>
2022-12-02 18:15:47 -0600

    search bar

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use eframe::{egui::{CentralPanel,ScrollArea,Context}, run_native, NativeOptions};
+use eframe::{egui::{CentralPanel,ScrollArea,TopBottomPanel,Ui,Context}, run_native, NativeOptions};
 use std::fs;
 use std::fs::metadata;
 use egui::widgets::Link;
+//use std::sync::mpsc;
+//use std::thread;
 
-const APP_NAME: &str = "Archivos";
+const APP_NAME: &str = "Files";
 
 struct MyFiles {
     path:String,
-    prev_path:String
+    prev_path:String,
+    search:String
 }
 
 impl MyFiles {
@@ -15,22 +18,26 @@ impl MyFiles {
         if cfg!(windows) {
             Self {
             path:String::from("/"),
-            prev_path:String::from("/")
+            prev_path:String::from("/"),
+            search:"".to_owned(),
             }
         }else if cfg!(unix){
             Self {
             path:String::from("/"),
-            prev_path:String::from("/")
+            prev_path:String::from("/"),
+            search:"".to_owned(),
             }
         }else if cfg!(target_os = "macos"){
             Self {
             path:String::from("/"),
-            prev_path:String::from("/")
+            prev_path:String::from("/"),
+            search:"".to_owned(),
             }
         }else{
             Self {
             path:String::from("/"),
-            prev_path:String::from("/")
+            prev_path:String::from("/"),
+            search:"".to_owned(),
             }
 
         }
@@ -48,18 +55,16 @@ impl eframe::App for MyFiles{
                     let file_path =file.as_ref().unwrap().path().to_str().unwrap().to_string().to_owned();
                     let linku = Link::new(file_path.to_owned());
 
-                    if is_dir(file_path.to_owned()){
-                        if ui.link(file_path.to_owned()).clicked(){
-                            self.prev_path=self.path.to_owned();
-                            self.path = file_path.to_owned();
-                        }
-                    ui.separator();
+                    if self.search.len()==0{
+                        render_files(file_path,linku,ui,&mut self.prev_path,&mut self.path);
                     }else{
-                        if ui.add(linku).clicked(){
-                            println!("{}",file_path);
+                        if file_path.contains(&self.search){
+                        render_files(file_path,linku,ui,&mut self.prev_path, &mut self.path);
                         }
-                    ui.separator();
-                }}
+                    }}
+            });
+        TopBottomPanel::bottom("lalalaal aqui valo de escribir mensajes").show(ctx, |ui| {
+            ui.with_layout(egui::Layout::top_down_justified(egui::Align::Center), |ui| ui.text_edit_multiline(&mut self.search));
             });
         });
     }
@@ -68,13 +73,13 @@ impl eframe::App for MyFiles{
 pub fn run_window(){
 
     let options = NativeOptions::default();
-    let app =MyFiles::new();
-
+    let app = MyFiles::new();
     run_native(
         APP_NAME,
         options,
-                Box::new(|_cc| Box::new(app)),
+        Box::new(|_cc| Box::new(app)),
     );
+
 
 }
 
@@ -86,3 +91,22 @@ fn is_dir(value:String)->bool{
         false
     }
 }
+
+fn render_files(file_path:String, linku:Link, ui:&mut Ui, prev_path:&mut String, path:&mut String){
+    if is_dir(file_path.to_owned()){
+        if ui.add(linku).clicked(){
+            *prev_path = path.to_owned();
+            *path = file_path.to_owned();
+        }
+        ui.separator();
+    }else{
+        if ui.add(linku).clicked(){
+            println!("{}",file_path);
+        }
+        ui.separator();
+    }
+}
+
+ fn main() {
+     run_window()
+ }
