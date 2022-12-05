8e020d0dd538f669b2fe0edd1306c154b2a4d36f
senchi <senchpimy@gmail.com>
2022-12-05 10:06:34 -0600

    paths instad of strings

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::fs::metadata;
 use egui::widgets::Link;
 use std::env;
 use directories::BaseDirs;
+use std::path::PathBuf;
 
 const APP_NAME: &str = "Files";
-//const IMAGES:Vec<&str> = vec!["png","jpg","jpeg","gif"];
-//const VIDEOS:Vec<&str> = vec!["mp4","webm"];
-//const DOCUMENTS:Vec<&str> =vec!["doc","docx","ppt","html","pdf","xml"];
 
 pub struct MyFiles {
     path:String,
@@ -19,15 +17,12 @@ pub struct MyFiles {
 
 impl MyFiles {
     fn new(ext_vec:Vec<String>)-> MyFiles {
-    println!("{:?}",&ext_vec);
-    println!("creado");
             Self {
             path:BaseDirs::new().unwrap().home_dir().to_str().unwrap().to_string(),
-            prev_path:String::from("/"),
+            prev_path:BaseDirs::new().unwrap().home_dir().parent().unwrap().to_str().unwrap().to_string(),
             search:"".to_owned(),
             type_to_search:ext_vec,
             }
-
     }
 }
 
@@ -35,7 +30,8 @@ impl eframe::App for MyFiles{
     fn update(&mut self, ctx:  &Context, frame: &mut eframe::Frame){
         CentralPanel::default().show(ctx, |ui| {
             if ui.button("return").clicked(){
-                self.path=self.prev_path.to_owned();
+                self.path = self.prev_path.to_owned();
+                self.prev_path = PathBuf::from(&self.path).parent().unwrap().to_str().unwrap().to_string();
             }
             ScrollArea::vertical().show(ui, |ui| {
                 for file in fs::read_dir(&self.path).unwrap() {
@@ -48,7 +44,7 @@ impl eframe::App for MyFiles{
                             }}
                 }
             });
-        TopBottomPanel::bottom("lalalaal aqui valo de escribir mensajes").show(ctx, |ui| {
+        TopBottomPanel::bottom("search_bar").show(ctx, |ui| {
             ui.with_layout(egui::Layout::top_down_justified(egui::Align::Center), |ui| ui.text_edit_multiline(&mut self.search));
             });
         });
@@ -75,8 +71,8 @@ fn is_dir(value:String)->bool{
 
 fn render_files(file_path:String, ui:&mut Ui, prev_path:&mut String, path:&mut String,frame: &mut eframe::Frame,extensions:&Vec<String>){
     if is_dir(file_path.to_owned()){
-        if ui.add(Link::new(file_path.to_owned())).clicked(){
-            *prev_path = path.to_owned();
+        if ui.add(Link::new(&file_path)).clicked(){
+            *prev_path = PathBuf::from(&file_path).parent().unwrap().to_str().unwrap().to_string();
             *path = file_path.to_owned();
         }
         ui.separator();
@@ -102,8 +98,6 @@ fn render_files(file_path:String, ui:&mut Ui, prev_path:&mut String, path:&mut S
  fn main() {
     let args: Vec<String> = env::args().collect();
     let args_len = args.len();
-    println!("{:?}",&args);
-    println!("{}",args_len);
     if args_len>2{
         let app = MyFiles::new(args[1..args.len()].to_vec());
         run_window(app);
@@ -111,6 +105,8 @@ fn render_files(file_path:String, ui:&mut Ui, prev_path:&mut String, path:&mut S
         let extensions:Vec<String>;
         match args[1].as_str() {
             "images" => extensions = vec!["png".to_string(),"jpg".to_string(),"jpeg".to_string(),"gif".to_string()],
+            "videos" => extensions = vec!["mp4".to_string(),"webm".to_string()],
+            "documents" => extensions =vec!["doc".to_string(),"docx".to_string(),"pdf".to_string(),"ppt".to_string()],
            _ => extensions = vec![args[1].to_string()]  ,
         }
         let app = MyFiles::new(extensions);
